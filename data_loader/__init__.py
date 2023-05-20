d7d5bad52ae8fff39eea06628fb900b90e1162ce
Jens <jens.jens@live.dk>
2023-05-20 15:28:40 +0200

    feat(dataset): removed everything to do with kitti

--- data_loader/__init__.py
+++ data_loader/__init__.py
@@ -1,3 +1,3 @@
 from data_loader.waymo_loader import WaymoLoader
-from data_loader.kitti_loader import KittiLoader
+# from data_loader.kitti_loader import KittiLoader
 from data_loader.nuscenes_loader import NuScenesLoader, NuScenesLoader10Hz
